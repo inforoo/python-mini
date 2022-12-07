d75a49804f4d61f222d2e53a67d65d9ff70e2cf0
yury <andreev_yurij@mail.ru>
2022-12-07 19:41:12 +0300

    venv added

---
+++ venv/converter.py
@@ -0,0 +1,21 @@
+from sys import argv
+from PIL import Image
+from pylab import *
+
+def main():
+    img_name = argv[1]
+    img = Image.open(img_name)
+    img = img.convert('L')
+    img.save('gray_'+img_name)
+    #img.show()
+
+    #im=array(img)
+    #figure()
+    #hist(im.flatten(), 16)
+    #show()
+
+    print("well done!")
+
+if __name__ == "__main__":
+    main()
+
